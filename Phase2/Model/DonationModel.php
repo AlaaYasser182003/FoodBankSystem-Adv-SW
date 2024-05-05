8cd38c34abcda91d12987ec1f3ad0a36bc2869dc
AlaaYasser182003 <Alaay318@gmail.com>
2024-05-05 19:36:55 +0300

    hashed id modification

--- Phase2/Model/DonationModel.php
+++ Phase2/Model/DonationModel.php
@@ -15,21 +15,51 @@ class DonationModel extends ModifiableAbstModel {
         $this->donation_date = $donation_date;
     }
 
+    
+
     public function add() {
         global $pdo;
+        
         $sql = "INSERT INTO ".self::table." (donor_id, total_cost, donation_date)
-        VALUES (:donor, :cost, :date)";
+                VALUES (:donor, :cost, :date)";
         $stmt = $pdo->prepare($sql);
-        return $stmt->execute(array(
+        $stmt->execute(array(
             'donor' => $this->donor_id,
             'cost' => $this->total_cost,
-            'date' => $this->donation_date,
+            'date' => $this->donation_date
         ));
+       
+        $lastInsertedId = $pdo->lastInsertId();
+
+        $md5Hash = md5($lastInsertedId);
+        
+        $sql = "UPDATE ".self::table." SET donationid = :md5Hash WHERE id = :lastInsertedId";
+        $stmt = $pdo->prepare($sql);
+       return  $stmt->execute(array(
+            ':md5Hash' => $md5Hash,
+            ':lastInsertedId' => $lastInsertedId
+        ));
+       
     }
+
+public static function getDonationId($donor_id, $donation_date) {
+    global $pdo;
+    
+    $sql = "SELECT donationid FROM ".self::table." WHERE donor_id = :donor_id AND donation_date = :donation_date";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([
+        'donor_id' => $donor_id,
+        'donation_date' => $donation_date
+    ]);
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    
+    return $row['donationid'];
+}
+    
 
     public function read() {
         global $pdo;
-        $sql = "SELECT * FROM ".self::table." WHERE id = :id";
+        $sql = "SELECT * FROM ".self::table." WHERE donationid = :id";
         $stmt = $pdo->prepare($sql);
         $stmt->execute(['id' => $this->id]);
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
@@ -41,7 +71,7 @@ class DonationModel extends ModifiableAbstModel {
 
     public function edit() {
         global $pdo, $table;
-        $sql = "UPDATE ".self::table." SET donor_id = :donor, total_cost = :cost, donation_date = :date WHERE id = :id";
+        $sql = "UPDATE ".self::table." SET donor_id = :donor, total_cost = :cost, donation_date = :date WHERE donationid = :id";
         $stmt = $pdo->prepare($sql);
         return $stmt->execute([
             'id' => $this->id,
@@ -53,7 +83,7 @@ class DonationModel extends ModifiableAbstModel {
 
     public static function remove($id) {
         global $pdo, $table;
-        $sql = "DELETE FROM ".self::table." WHERE id = :id";
+        $sql = "DELETE FROM ".self::table." WHERE donationid = :id";
         $stmt = $pdo->prepare($sql);
         return $stmt->execute(['id' => $id]);
     }
@@ -61,6 +91,14 @@ class DonationModel extends ModifiableAbstModel {
     public static function view_all(){
         global $pdo;
         $stmt = $pdo->query("SELECT * FROM ".self::table);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public static function view_all_donor($donor_id){
+        global $pdo;
+        $sql = "SELECT * FROM ".self::table." WHERE donor_id = :donor_id";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(['donor_id' => $donor_id]);
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
@@ -103,5 +141,9 @@ class DonationModel extends ModifiableAbstModel {
     }
     public function setDate($date){
         $this->donation_date = $date;
+    }
+    public static function getLastInsertedId() {
+        global $pdo; // Assuming $pdo is your database connection object
+        return $pdo->lastInsertId();
     }
 }
