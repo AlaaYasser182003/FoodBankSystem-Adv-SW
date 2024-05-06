4b89e3b8143471bbfb67c7c07336555af24ff591
AO2400 <166804338+AO2400@users.noreply.github.com>
2024-05-06 14:43:21 +0300

    Add files via upload

--- Phase2/Model/pdo.php
+++ Phase2/Model/pdo.php
@@ -1,3 +1,20 @@
 <?php
-    $pdo = new PDO('mysql:host=localhost;port=3306;dbname=foodbank',
-    'root', '');
+    Class Singleton {
+        private static $pdo;
+        public static function getpdo(){
+            if (self :: $pdo == null) {
+                self :: $pdo = new PDO('mysql:host=localhost;port=3306;dbname=foodbank','root', '');
+        }
+        return self :: $pdo;
+    }
+}
+
+
+
+
+
+
+
+
+
+
