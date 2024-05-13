3b83b11c05844febf4a544ad761d9c9993b4ceda
omar-sameh-mokhtar <166794956+omar-sameh-mokhtar@users.noreply.github.com>
2024-05-13 03:37:41 +0300

    minor change in DonationController.php

--- Phase2/Controller/DonationController.php
+++ Phase2/Controller/DonationController.php
@@ -14,7 +14,7 @@ class DonationController {
         $donationModel = new DonationModel(md5($_SESSION['user_id']), $_GET['cost'], date('y-m-d'));
         $donationModel->add(); 
     
-        $donationid = DonationModel::getDonationId(md5($_SESSION['user_id']), date('y-m-d'));
+         $donationid = $donationModel->getDonationId(md5($_SESSION['user_id']), $_GET['cost'],date('y-m-d'));
         
         header("Location: DonationDetailsController.php?cmd=add&id=".$donationid); 
         return;    
