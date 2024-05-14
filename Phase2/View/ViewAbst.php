8b02e048cb809a332596cd2415e9456e6f069908
AlaaYasser182003 <Alaay318@gmail.com>
2024-05-14 21:15:56 +0300

    minor change

--- Phase2/View/ViewAbst.php
+++ Phase2/View/ViewAbst.php
@@ -11,10 +11,15 @@ class ViewAbst {
         </body>
         </html>");
     }
-    function PrintMessage($succ) {
-        if ($succ)
+    function PrintMessage($succ, $method =null) {
+        if ($succ){
             echo('<p style="color: green; text-align: center; font-size:large;">
             Operation was Successfull !</p>');
+    if($method != null)
+       {     echo('<p style="color: green; text-align: center; font-size:large;">
+            using '.$method.' </p>');
+        }
+    }
         else echo('<p style="color: red; text-align: center; font-size:large;">
             Operation was not Executed !</p>');
     }
